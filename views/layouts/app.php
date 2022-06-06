5f9a182b29a647c33c8728950f78152e69727573
Sergio Fernandoy carrasco <macdesergio@MacBook-Air-de-Sergio.local>
2022-06-06 12:50:35 -0400

    cambios en el css, agregue clases, idente el codigo

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -11,63 +11,66 @@
 </head>
 
 <body>
+  <nav class="navbar navbar-expand-lg" style="background-color:#017548">
+    <div class="container">
+      
+      <a class="navbar-brand" href="#">
+        <img src="assets/Img/st-logo.png" alt="" width="80" height="80">
+      </a>
 
-<nav class="navbar navbar-expand-lg" style="background-color:#017548">
-  <div class="container">
-     <a class="navbar-brand" href="#">
-      <img src="assets/Img/st-logo.png" alt="" width="80" height="80">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto ">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="?url=home">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="?url=Productos">Productos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="?url=CRUD">CRUD Productos</a>
-        </li>
-      </ul>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto ">
+
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="?url=home">Inicio</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="?url=Productos">Productos</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="?url=CRUD">CRUD Productos</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container ">
+    <div class="row">
+      <?php
+        $url = (isset($_GET['url'])) ? $_GET['url'] : 'home';
+        switch($url)
+          {
+            case 'home'     : SitioController::home();
+                            break;
+            case 'Productos': SitioController::Productos();
+                            break;
+            case 'CRUD'     : SitioController::CRUD();
+                            break;
+            default         : SitioController::index();
+                            break;
+          }
+    ?>    
     </div>
   </div>
-</nav>
-
-    <div class="container ">
-            <div class="row">
-
-                <?php
-                $url = (isset($_GET['url'])) ? $_GET['url'] : 'home';
-                switch($url)
-                {
-                    case 'home'    : SitioController::home();
-                                    break;
-                    case 'Productos': SitioController::Productos();
-                                    break;
-                    case 'CRUD'     : SitioController::CRUD();
-                                    break;
-                    default         : SitioController::index();
-                                    break;
-                }
-                ?>
-            </div>
+<footer>
+  <div class="position-relative">
+    <div class="position-absolute bottom- start-50 translate-middle">
+      <p>
+        Sitio creado por: Sergio F. y Victor V. &copy; <?php echo date('Y'); ?>
+      </p>
     </div>
-    <footer>
-
-        <div class="position-relative">
-            <div class="position-absolute bottom- start-50 translate-middle">
-                <p>
-                    Sitio creado por: Sergio F. y Victor V. &copy; <?php echo date('Y'); ?>
-                </p>
-            </div>
-        </div>
-
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <script src="assets/js/main.js"></script>
+  </div>
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <script src="assets/js/main.js"></script>
 </body>
 </html>
 
