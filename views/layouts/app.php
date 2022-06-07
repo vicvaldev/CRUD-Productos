34b735e8666ec285593b01e3b17c8d9bccb4384a
Victor Valdivia <victor.valdivia.dev@gmail.com>
2022-06-07 14:12:39 -0400

    buscador sin recargar no funciona D:

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -54,7 +54,7 @@
                             break;
             case 'CRUD'     : SitioController::CRUD();
                             break;
-            default         : SitioController::index();
+            default         : SitioController::home();
                             break;
           }
     ?>    
@@ -69,6 +69,9 @@
     </div>
   </div>
   </footer>
+      <!-- AJAX -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxy/1.6.1/scripts/jquery.ajaxy.min.js"></script>
+  <script src="assets/js/ajax.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   <script src="assets/js/main.js"></script>
 </body>
--- views/modules/productos.php
+++ views/modules/productos.php
@@ -5,30 +5,12 @@
     </h1>
     <div class="container my-4">
         <div class="col-4 ms-auto">
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Buscar</button>
-            </form>
+            <input id="buscar" name="buscar"class="form-control me-2" type="text" placeholder="Buscar...">
         </div>
     </div>
-    <table class="table table-success table-striped table-hover">
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Precio</th>
-            <th>Categoria</th>
-        </tr>
-        <?php
-            $producto = new ProductoModel();
-            $lista = $producto->getProductos();
-            foreach ($lista as $p) {
-            echo "<tr>";
-            echo "<td>".$p['id']."</td>";
-            echo "<td>".$p['nombre']."</td>";
-            echo "<td>".$p['precio']."</td>";
-            echo "<td>".$p['categoria']."</td>";
-            echo "</tr>";
-            }
+    <table class="table table-success table-striped table-hover" id="tabla-productos">
+        <?php 
+            echo ProductoModel::getProducto();
         ?>
     </table>
 
